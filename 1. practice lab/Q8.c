e5424092b4e8495cb90c1405b064f09d2b16f13f
DilnaJoseph <dilnamjoseph@gmail.com>
2025-07-24 21:04:41 +0530

    Create Q8.c

---
+++ 1. practice lab/Q8.c
@@ -0,0 +1,68 @@
+/*
+Q8. Add Two Numbers Represented by Linked Lists
+Two numbers are stored in reverse order as linked lists; add them and return the sum
+as a new linked list.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h> 
+#include<ctype.h>
+
+struct node{
+    int val;
+    struct node* next,*prev;
+};
+
+struct node* root = NULL;
+
+struct node* create(int val){
+    struct node* new = (struct node*)malloc(sizeof(struct node));
+    new->val=val;
+    new->next=new->prev=NULL;
+    return new;
+}
+
+void add_elements(int val){
+    struct node* new = create(val);
+    if(!root){
+        root = new;
+        return;
+    }
+    struct node* temp = root;
+    while(temp->next){
+        temp=temp->next;
+    }
+    temp->next = new;
+    new->prev=temp;
+}
+
+int main(){
+    int n,k,m;
+    printf("Enter the number of nodes\n");
+    scanf(" %d",&k);
+    
+    int l;
+    for(int i=0;i<k;i++){
+        scanf(" %d",&l);
+        add_elements(l);
+    }
+
+    struct node *first=root, *last=NULL, *temp=root;
+    
+    while(temp){
+        last = temp;
+        temp=temp->next;
+    }
+
+    for(int i=0;i<n/2;i++){
+    if(first->val!=last->val){
+        printf("Not a palindrome.\n");
+        return;
+    }
+    first = first->next;
+    last = last->prev;
+    }
+    printf("It is a palindrome.\n");
+    return 0;
+}
